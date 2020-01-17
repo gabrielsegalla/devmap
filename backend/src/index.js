042c6d6e2d1d0a502bbdd82eff841b3e4129f085
gabrielsegalla <gabriel.segalla@agrosatelite.com.br>
2020-01-17 19:07:14 -0300

    finalizando semana omnistack com mobile

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
+const http = require('http')
 const routes = require('./routes')
-
+const { setupWebsocket } = require('./websocket')
 const app = express();
+
+const server = http.Server(app)
+
+
+setupWebsocket(server);
 
 //mongoose.connect('mongodb+srv://gabrielsegalla:gabrielsegalla@cluster0-onmna.mongodb.net/devmap?retryWrites=true&w=majority', {
 mongoose.connect('mongodb://gabrielsegalla:gabrielsegalla@cluster0-shard-00-00-onmna.mongodb.net:27017,cluster0-shard-00-01-onmna.mongodb.net:27017,cluster0-shard-00-02-onmna.mongodb.net:27017/devmap?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
@@ -15,4 +21,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
